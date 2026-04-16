fed1f5b63e3e0a2c4f012d3c0d2d6fa86d3b5329
Viswanadhsai <viswanadhsai35@gmail.com>
2026-04-16 14:38:16 +1000

    5.D SAFE WRITES IMPLEMENTATION

--- 5.4D/routes/bookRoutes.js
+++ 5.4D/routes/bookRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/bookController");
 
-// URL mapping only. No business logic here.
 router.get("/", controller.getBooks);
 router.get("/:id", controller.getBook);
 router.post("/", controller.createBook);
